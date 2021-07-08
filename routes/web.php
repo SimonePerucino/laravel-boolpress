0c6e249486323dc16d9781df27a35a63d39f72be
SimonePerucino <simone.perucino@gmail.com>
2021-07-09 01:15:11 +0200

    quarta bozza

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', "HomeController@index");
+Route::get('/', "HomeController@index")->name("index");
+Route::get('/posts', "PostController@index")->name("posts.index");
 
 
 Auth::routes();
@@ -24,8 +25,11 @@ Auth::routes();
 Route::prefix("admin")
     ->namespace("Admin")
     ->middleware("auth")
+    ->name("admin.")
     ->group(function(){
             Route::get("/", "HomeController@index")->name("home");
+            // Route::get("/posts", "PostController@index")->name("home");
+            Route::resource("/posts", "PostController");
     });
 
     Route::prefix("users")
